119c9e05c3d5ce5047e31852cfe33b7c50813a9f
mario.freund <mario.freund@student.hpi.uni-potsdam.de>
2023-04-12 11:14:49 +0200

    add road dataset

--- semi-supervised-1.15/preprocessing.py
+++ semi-supervised-1.15/preprocessing.py
@@ -58,6 +58,45 @@ def preprocess(file_name):
     print('#Attack: ', df[df['label'] == 1].shape[0])
     return df[['features', 'label']].reset_index().drop(['index'], axis=1)
 
+def preprocess_altformat(file_name, total_normal, total_attack):
+    df = dd.read_csv(file_name, dtype={
+        'label': bool,
+        'timestamp': float, 
+        'id': str,
+        'dlc': int,
+        'data0': str,
+        'data1': str,
+        'data2': str,
+        'data3': str,
+        'data4': str,
+        'data5': str,
+        'data6': str,
+        'data7': str})
+    print('Reading from {}: DONE'.format(file_name))
+    print('Dask processing: -------------')
+    pd_df = df.compute()
+    pd_df = pd_df[['label','timestamp','id']].sort_values('timestamp',  ascending=True)
+    pd_df['id'] = pd_df.id.apply(convert_canid_bits)
+    print('Dask processing: DONE')
+    print('Aggregate data -----------------')
+    as_strided = np.lib.stride_tricks.as_strided  
+    win = 29
+    s = 29
+    #Stride is counted by bytes
+    feature = as_strided(pd_df.id, ((len(pd_df) - win) // s + 1, win), (8*s, 8)) 
+    label = as_strided(pd_df.label, ((len(pd_df) - win) // s + 1, win), (1*s, 1))
+    df = pd.DataFrame({
+        'features': pd.Series(feature.tolist()),
+        'label': pd.Series(label.tolist())
+    }, index= range(len(feature)))
+    df['label'] = df['label'].apply(lambda x: 1 if any(x) else 0)
+    print('Preprocessing: DONE')
+    print('#Normal: ', df[df['label'] == 0].shape[0])
+    total_normal = total_normal + df[df['label'] == 0].shape[0]
+    print('#Attack: ', df[df['label'] == 1].shape[0])
+    total_attack = total_attack + df[df['label'] == 1].shape[0]
+    return df[['features', 'label']].reset_index().drop(['index'], axis=1), total_normal, total_attack
+
 def serialize_example(x, y):
     """converts x, y to tf.train.Example and serialize"""
     #Need to pay attention to whether it needs to be converted to numpy() form
@@ -102,12 +141,74 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--indir', type=str, default="./Data/Car-Hacking")
     parser.add_argument('--outdir', type=str, default="./Data/TFRecord/")
-    parser.add_argument('--attack_type', type=str, default="all", nargs='+')
+    parser.add_argument('--attack_type', type=str, default='hcrl')
     args = parser.parse_args()
     
-    if args.attack_type == 'all':
+    if args.attack_type == 'hcrl':
         attack_types = ['DoS', 'Fuzzy', 'gear', 'RPM']
+    elif (args.attack_type == 'tu'):
+        attack_types = ['diagnostic', 'dosattack', 'fuzzing_canid', 'fuzzing_payload', 'replay']
+    elif (args.attack_type == 'road_without_masquerade'):
+        attacks_types = ['correlated_signal_attack_1',
+                        'correlated_signal_attack_2',
+                        'correlated_signal_attack_3',
+                        'fuzzing_attack_1',
+                        'fuzzing_attack_2',
+                        'fuzzing_attack_3',
+                        'max_speedometer_attack_1',
+                        'max_speedometer_attack_2',
+                        'max_speedometer_attack_3',
+                        'reverse_light_off_attack_1',
+                        'reverse_light_off_attack_2',
+                        'reverse_light_off_attack_3',
+                        'reverse_light_on_attack_1',
+                        'reverse_light_on_attack_2',
+                        'reverse_light_on_attack_3'
+                        ]
+    elif (args.attack_type == 'road_with_masquerade'):
+        attack_types = ['correlated_signal_attack_1_masquerade',
+                        'correlated_signal_attack_1',
+                        'correlated_signal_attack_2_masquerade',
+                        'correlated_signal_attack_2',
+                        'correlated_signal_attack_3_masquerade',
+                        'correlated_signal_attack_3',
+                        'fuzzing_attack_1',
+                        'fuzzing_attack_2',
+                        'fuzzing_attack_3',
+                        'max_engine_coolant_temp_attack_masquerade',
+                        'max_speedometer_attack_1',
+                        'max_speedometer_attack_2_masquerade',
+                        'max_speedometer_attack_2',
+                        'max_speedometer_attack_3_masquerade',
+                        'max_speedometer_attack_3',
+                        'reverse_light_off_attack_1_masquerade',
+                        'reverse_light_off_attack_1',
+                        'reverse_light_off_attack_2_masquerade',
+                        'reverse_light_off_attack_2',
+                        'reverse_light_off_attack_3_masquerade',
+                        'reverse_light_off_attack_3',
+                        'reverse_light_on_attack_1_masquerade',
+                        'reverse_light_on_attack_1',
+                        'reverse_light_on_attack_2_masquerade',
+                        'reverse_light_on_attack_2',
+                        'reverse_light_on_attack_3_masquerade',
+                        'reverse_light_on_attack_3'
+                        ]
+    elif (args.attack_type == 'road_just_masquerade'):
+        attack_types = ['correlated_signal_attack_1_masquerade',
+                        'correlated_signal_attack_2_masquerade',
+                        'correlated_signal_attack_3_masquerade',
+                        'max_engine_coolant_temp_attack_masquerade',
+                        'max_speedometer_attack_2_masquerade',
+                        'max_speedometer_attack_3_masquerade',
+                        'reverse_light_off_attack_1_masquerade',
+                        'reverse_light_off_attack_2_masquerade',
+                        'reverse_light_off_attack_3_masquerade',
+                        'reverse_light_on_attack_1_masquerade',
+                        'reverse_light_on_attack_2_masquerade',
+                        'reverse_light_on_attack_3_masquerade',
+                        ]
     else:
         attack_types = [args.attack_type]
-     
+
     main(args.indir, args.outdir, attack_types)
