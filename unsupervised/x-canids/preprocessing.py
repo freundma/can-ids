7c14d5e648d9603b12f0d3d62626f01fd19c3c6e
mario.freund <mario.freund@student.hpi.uni-potsdam.de>
2023-05-03 16:05:38 +0200

    started working on caching for incr performance

--- unsupervised/x-canids/preprocessing.py
+++ unsupervised/x-canids/preprocessing.py
@@ -1,11 +1,14 @@
 # Date: 05-02-2023
 # Author: Mario Freund
-# Purpose: Preprocess ambient signal extracted data of road as done in x-canids
+# Purpose: Preprocess ambient signal extracted data of road dataset as done in x-canids
 
 import pandas as pd
 import numpy as np
 import tensorflow as tf
 import argparse
+from tqdm import tqdm
+import sys
+np.set_printoptions(threshold=sys.maxsize)
 
 def compute_offsets(offsets):
     for i in range(1, len(offsets)):
@@ -13,48 +16,67 @@ def compute_offsets(offsets):
     return offsets
 
 def scale_s(s, unique_id_list, min_dict, max_dict):
-    scaled_s = s
-    i = 0
-    for s_i in np.nditer(s):
-        min_i = min_dict[str(unique_id_list[i])]
-        max_i = max_dict[str(unique_id_list[i])]
-        scaled_s[i] = (s_i - min_i) / (max_i - min_i)
-        i += 1
+    scaled_s = s # copy
+    offset = 0
+    for i in range(len(unique_id_list)):
+        # get minimums and maximums of signal of each ID
+        mins_i = min_dict[str(unique_id_list[i])]
+        maxs_i = max_dict[str(unique_id_list[i])]
+        for j in range(len(mins_i)): # scale
+            if(maxs_i[j] == mins_i[j]):
+                scaled_s[offset+j] = 1.0
+            else:
+                scaled_s[offset+j] = (scaled_s[offset+j] - mins_i[j]) / (maxs_i[j] - mins_i[j]) # s^_i  = (s_i - min_i) / (max_i - min_i)
+        offset += len(mins_i)
     return scaled_s
 
-def get_s(df, t, offsets, unique_id_list, min_dict, max_dict):
-    df_t = df.loc[df['Time'] <= t]
-    unique_id_list_t = df_t['ID'].unique().tolist()
-    if(len(unique_id_list) != len(unique_id_list_t)):
-        return None # have to see each ID at least once
+def get_s(df, t, delta_t, offsets, unique_id_list, min_dict, max_dict, cache, all_seen):
+    clean = True
+    t_minus_one = t - delta_t
+    df_t = df.loc[(df['Time'] <= t) & (df['Time'] > t_minus_one)]
     # get latest signals of each ID
-    s = np.empty(sum(offsets)) # total amount of signals
+    s = np.empty(offsets[-1]) # total amount of signals
     i = 0
     offset = 0
     for id in unique_id_list:
         df_id = df_t.loc[df_t['ID'] == id]
-        df_id = df_id[df.Time == df.Time.max()] # latest value
-        df_id = df_id.drop(['Time','ID'])
+        #print(df_id)
+        if (df_id.empty): # take cached value
+            if (not all_seen):
+                clean = False
+            else:
+                s[offset:offsets[i]] = cache[str(id)]
+            offset = offsets[i]
+            i += 1
+            continue
+        index = df_id['Time'].idxmax() # latest value
+        df_id = df_id.loc[[index]]
+        df_id = df_id.drop(['Time','ID'], axis=1)
         df_id = df_id.dropna(axis=1)
-        signals = df.to_numpy.flatten()
+        signals = df_id.to_numpy().flatten()
+        cache[str(id)] = signals # cache signals
         s[offset:offsets[i]] = signals
-        offset += offsets[i]
+        offset = offsets[i]
         i += 1
-    
-    return scale_s(s, unique_id_list, min_dict, max_dict)
+
+    if (clean):
+        return scale_s(s, unique_id_list, min_dict, max_dict), cache
+    else:
+        return None, cache
 
 def range_of_signals(df, id):
     df_id = df.loc[df['ID']==id]
-    df_id = df_id.drop('ID')
+    df_id = df_id.drop(['Time','ID'], axis=1)
     df_id = df_id.dropna(axis=1) 
-    # just relevant columns left
-    mins, maxs = []
+    # just relevant signal columns left
+    mins = []
+    maxs = []
     for col in df_id:
         maxs.append(df_id[col].max())
         mins.append(df_id[col].min())
     return mins, maxs
 
-def main(inputfile, outdir, t, w):
+def main(inputfile, outfile, delta_t, w):
     df = pd.read_csv(inputfile, dtype={
         'Label': bool,
         'Time': float,
@@ -83,15 +105,18 @@ def main(inputfile, outdir, t, w):
         'Signal_22_of_ID': float,
     })
 
-    df = df.drop('Label')
-    unique_id_list = df['ID'].unique().tolist().sort()
+    df = df.drop(['Label'], axis=1)
+    unique_id_list = df['ID'].unique()
+    unique_id_list.sort()
+    unique_id_list = list(unique_id_list)
 
-    # get min max of each 
     min_dict = {}
     max_dict = {}
+    cache = {}
     offsets = []
-    max_t = df['Time'].max
+    max_t = df['Time'].max()
     
+    # get min max of each
     for id in unique_id_list:
         mins, maxs = range_of_signals(df, id)
         min_dict[str(id)] = mins
@@ -100,30 +125,40 @@ def main(inputfile, outdir, t, w):
 
     offsets = compute_offsets(offsets)
 
+    # prepare writing to disk
+    results_path = outfile + ".tfrecords"
+    writer = tf.io.TFRecordWriter(results_path)
+
     # loop over time steps
     steps = 0
-    for t in range(0, max_t, t):
-        steps += 1
-        s = get_s(df, t, offsets, unique_id_list, min_dict, max_dict)
-        if ((steps % w) == 0):
-            # TODO
+    s_w = np.empty([w, offsets[-1]])
+    all_seen = False
+    for t in ((np.arange(delta_t, max_t, delta_t))):
+        if (((steps % w) == 0) and (steps > 0)): # wait until we have w vectors
+            s_w = np.reshape(s_w, w*offsets[-1]) # e.g. 200*664
+            # write to TF_Record
+            example = tf.train.Example(features=tf.train.Features(feature={
+                'X': tf.train.Feature(float_list=tf.train.FloatList(value=s_w))
+            }))
+            writer.write(example.SerializeToString())
+            s_w = np.empty([w, offsets[-1]])
 
+        s, cache = get_s(df, t, delta_t, offsets, unique_id_list, min_dict, max_dict, cache, all_seen)
+        print(s)
+        print("-------------------------------------------")
+        if (not(s is None)):
+            all_seen = True
+            s_w[steps % w] = s
+            steps += 1
 
-
-
-
-
-
-
-
-    return None
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--infile', type=str, default="./ambient_street_driving_long.csv")
-    parser.add_argument('--outdir', type=str, default="./")
+    parser.add_argument('--outfile', type=str, default="./")
     parser.add_argument('--timesteps', type=float, default=0.01)
-    parser.add_argument('--windowsize', type=int, default=40)
+    parser.add_argument('--windowsize', type=int, default=200)
     args = parser.parse_args()
 
-    main(args.infile, args.outdir, args.timesteps, args.windowsize)
+    main(args.infile, args.outfile, args.timesteps, args.windowsize)
